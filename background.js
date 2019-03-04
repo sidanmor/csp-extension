dccdca55733469e188c60c988813f1cdec23345b
Idan Mor <sidanmor@gmail.com>
2019-03-04 17:03:56 +0200

    Indentations

--- background.js
+++ background.js
@@ -1,18 +1,14 @@
 function isCSPHeader(headerName) {
   return (headerName === 'CONTENT-SECURITY-POLICY') || (headerName === 'X-WEBKIT-CSP');
 }
-
-// Listens when new request
+// Listens on new request
 chrome.webRequest.onHeadersReceived.addListener((details) => {
   for (let i = 0; i < details.responseHeaders.length; i += 1) {
     if (isCSPHeader(details.responseHeaders[i].name.toUpperCase())) {
-
       const csp = 'default-src * \'unsafe-inline\' \'unsafe-eval\' data: blob:; ';
-      
       details.responseHeaders[i].value = csp;
     }
   }
-
   return { // Return the new HTTP header
     responseHeaders: details.responseHeaders,
   };
