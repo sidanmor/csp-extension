8d0e7fb5ddf8b06c1057daca981193b154a655be
Idan Mor <sidanmor@gmail.com>
2019-03-04 16:56:07 +0200

    Bypass all traffic

--- background.js
+++ background.js
@@ -6,17 +6,9 @@ function isCSPHeader(headerName) {
 chrome.webRequest.onHeadersReceived.addListener((details) => {
   for (let i = 0; i < details.responseHeaders.length; i += 1) {
     if (isCSPHeader(details.responseHeaders[i].name.toUpperCase())) {
-      let csp = details.responseHeaders[i].value;
 
-      // append "https://mysite.com" to the authorized sites
-      csp = csp.replace('script-src', 'script-src * ');
-      csp = csp.replace('style-src', 'style-src * ');
-      csp = csp.replace('connect-src', 'connect-src * ');
-      csp = csp.replace('img-src', 'img-src * ');
-      csp = csp.replace('font-src', 'font-src * ');
-      csp = csp.replace('media-src', 'media-src * ');
-      csp = csp.replace('default-src', 'media-src * ');
-
+      const csp = 'default-src * \'unsafe-inline\' \'unsafe-eval\' data: blob:; ';
+      
       details.responseHeaders[i].value = csp;
     }
   }
